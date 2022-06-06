ba363eb33efa3bfeb1f54a2152004066e55eb6c5
ASchwartzman <aleschwartzman22@gmail.com>
2022-06-05 22:18:09 -0300

    Sequence deployments

--- contracts/Helloworld.sol
+++ contracts/Helloworld.sol
@@ -3,8 +3,21 @@ pragma solidity ^0.8.4;
 
 contract Helloworld {
 
-    function hello() public pure returns (string memory) {
-        return "Helloworld";
+    string public message;
+    address owner;
+
+    constructor(string memory _message) {
+        message = _message;
+        owner = msg.sender;
     }
-    
+
+    function hello() public view returns (string memory) {
+        return message;
+    }
+
+    function setMessage(string memory _message) public payable {
+        // require(msg.value > 1 ether, 'You must pay at least 1 ETH');
+        require(msg.sender == owner, 'You are not the owner');
+        message = _message;
+    }
 }
