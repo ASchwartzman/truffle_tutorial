c5ef6fb5cc00e486d31f53f2b0743b8c554b2343
ASchwartzman <aleschwartzman22@gmail.com>
2022-06-09 18:51:49 -0300

    Test script added

--- test/helloworld_test.js
+++ test/helloworld_test.js
@@ -3,7 +3,7 @@ const Helloworld = artifacts.require("Helloworld");
 
 contract("Helloworld", (accounts) => {
     it("constructor should set the message correctly", async () => {
-        let instance = Helloworld.deployed();
+        let instance = await Helloworld.deployed();
         let message = await instance.message();
         assert.equal(message, "Hello constructor");
     })
